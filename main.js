f57673814b688a8f4157f153c59f2d55d8c9198a
haseebnaeem94 <162363763+haseebnaeem94@users.noreply.github.com>
2024-03-16 15:28:29 +0500

    Calculator
    This repository contains project of a calculator developed by using typescript.

---
+++ main.js
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    { name: "firstNumber", type: "number", message: "enter firstNumber" },
+    { name: "secondNumber", type: "number", message: "enter secondNumber" },
+    { name: "operator", type: "list", message: "selet operator to perform operation",
+        choices: ["Addition", "subtraction", "multiplication", "division"],
+    },
+]);
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("please select valid operator");
+}
